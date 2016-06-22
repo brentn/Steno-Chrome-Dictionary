8de21e09ceedcaac6132e717cc74d7dba1f1bb31
Brent <nesbitt.brent@gmail.com>
2016-06-22 13:15:18 -0700

    First working version

--- js/background.js
+++ js/background.js
@@ -11,39 +11,50 @@ chrome.runtime.onMessageExternal.addListener(function(request, sender, sendRespo
   if (request.action == "loadDictionaries") {
     var response = {status:"FAIL"};
     var dictionaries = [];
+    var numDicts = 0;
+    var numDictsLoaded=0;
     try {
     chrome.storage.sync.get({ DICTIONARIES: []}, function(items) {
-      items.DICTIONARIES.forEach(function(dictionary) {
-        chrome.fileSystem.restoreEntry(dictionary.id, function(fileEntry) {
-          if (fileEntry!==undefined) {
-            fileEntry.file(function(file) {
-              var reader = new FileReader();
-              reader.onerror = function(e) {
-                console.warn("FAIL");
-                sendResponse({"status":"FAIL"});
-              };
-              reader.onloadend = function(e) {
-                dictionaries.push({"name":fileEntry.name, "json":e.target.result});
-                console.log("Custom dictionary loaded: "+fileEntry.name);
-                if (dictionaries.length==items.DICTIONARIES.length) {
-                  console.debug("sending response");
-                  sendResponse({"status":"OK", "dictionaries":dictionaries});
-                }
-              };
-              reader.readAsText(file);
-            });
-            return true;
-          } else {
-            console.warn('no FileEntry found for '+dictionary.name);
-            sendResponse({"status":"FAIL - no FileEntry"});
-          }
-        });
-      });
+      numDicts=items.DICTIONARIES.length;
+      for (var i in items.DICTIONARIES) {
+        console.debug("Loading dictionary: "+items.DICTIONARIES[i].name);
+        loadDictionary(i, items.DICTIONARIES[i]);
+      }
     });
     } catch(ex) {
       console.warn("CATCH & FAIL");
       sendResponse({"status":"FAIL"});
     }
+    return true;
   }
+  
+  function loadDictionary(i, dictionary) {
+    chrome.fileSystem.restoreEntry(dictionary.id, function(fileEntry) {
+      if (fileEntry!==undefined) {
+        fileEntry.file(function(file) {
+          var reader = new FileReader();
+          reader.onerror = function(e) {
+            console.warn("FAIL");
+            sendResponse({"status":"FAIL"});
+          };
+          reader.onloadend = function(e) {
+            dictionaries[i] = {"name":fileEntry.name, "json":e.target.result};
+            numDictsLoaded++;
+            console.log("Custom dictionary loaded: "+fileEntry.name + "("+numDictsLoaded+")");
+            if (numDictsLoaded==numDicts) {
+              console.debug("sending response");
+              sendResponse({"status":"OK", "dictionaries":dictionaries});
+            }
+          };
+          reader.readAsText(file);
+        });
+      } else {
+        console.warn('no FileEntry found for '+dictionary.name);
+        sendResponse({"status":"FAIL - no FileEntry"});
+      }
+    });
+  }
+  
 });
+
 
